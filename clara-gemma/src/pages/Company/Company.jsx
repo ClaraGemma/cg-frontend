740a82afd90293762191f076518fb70374f2ba88
Matheus Romeiro <matheuscontrabaixoc4@gmail.com>
2024-06-13 13:45:24 -0300

    update_company
    atualização da página da empresa

--- clara-gemma/src/pages/Company/Company.jsx
+++ clara-gemma/src/pages/Company/Company.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NavbarResponsive from "../../components/NavbarResponsive/NavbarResponsive";
 import Footer from "../../components/Footer/Footer";
+import { MainContainer, ContentWrapper, Section, Header } from "./styles";
 
 function Empresa() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
@@ -13,27 +14,35 @@ function Empresa() {
         setMenuIsVisible={setMenuIsVisible}
       />
       <Navbar setMenuIsVisible={setMenuIsVisible} />
-      <div className="oloko">
-        <h1>Quem somos</h1>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </div>
+      <MainContainer>
+        <ContentWrapper>
+          <Section>
+            <Header>
+              <h1>Quem somos</h1>
+              <hr />
+            </Header>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Section>
 
-      <div className="">
-        <h1>Motivação</h1>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </div>
+          <Section>
+            <Header>
+              <h1>Motivação</h1>
+              <hr />
+            </Header>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Section>
+        </ContentWrapper>
+      </MainContainer>
       <Footer />
     </>
   );
---
+++ clara-gemma/src/pages/Company/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 11rem 5rem 5rem 5rem;
+`;
+
+export const ContentWrapper = styled.div`
+  max-width: 75%;
+`;
+
+export const Section = styled.section`
+  //   background-color: yellow;
+  padding: 5rem;
+
+  p {
+    color: var(--primary-text-color);
+    font-weight: 400;
+    font-size: 20px;
+    word-wrap: break-word;
+    margin-top: 1rem;
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+
+  h1 {
+    margin-right: 1rem;
+    color: var(--primary-text-color);
+    font-weight: 500;
+    font-size: 24px;
+  }
+
+  hr {
+    flex-grow: 1;
+    border: none;
+    height: 1px;
+    background-color: black;
+  }
+`;
